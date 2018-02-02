77ef6c1340ccec51053c07ff52d0051cff6bfa45
FrapperColin <colin.frapper@gmail.com>
2018-02-02 21:09:50 +0100

    Code avancer et reculer

---
+++ ProjectRileyRover/src/mouvement/MainClass.java
@@ -0,0 +1,24 @@
+package mouvement;
+
+import lejos.hardware.motor.Motor;
+import lejos.utility.Delay;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// avancer
+		Motor.A.forward();
+		Motor.B.forward();
+		Delay.msDelay(10000); // 10 secondes
+		Motor.A.stop();
+		Motor.B.stop();
+
+		// reculer
+		Motor.A.backward();
+		Motor.B.backward();
+		Delay.msDelay(10000); // 10 secondes
+		Motor.A.stop();
+		Motor.B.stop();
+	}
+
+}
